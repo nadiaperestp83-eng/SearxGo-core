b384d28fe487376ea0a548b11218d2d587a2b270
Claudio DeSouza <cdesouza@brave.com>
2026-06-06 16:41:04 +0100

    [cr151] `kWaitForFirstPartySetsInit` deleted
    This feature was already disabled in Brave, and now it is deleted from
    Chromium.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/23029b30d84957aee6a3346a175cf45b8085f19a
    
    commit 23029b30d84957aee6a3346a175cf45b8085f19a
    Author: Chris Fredrickson <cfredric@chromium.org>
    Date:   Fri Jun 5 12:00:08 2026 -0700
    
        [RWS] Remove kWaitForFirstPartySetsInit feature
    
        This removes the Related Website Sets feature that throttled navigations
        until RWS initialization was complete, and allowed the Network Service's
        RWS queries to resolve asynchronously, since that feature was never
        launched and was only used in tests. RWS itself is deprecated, so there
        are no plans to launch this feature and it can be deleted.
    
        This CL does not change any behavior (other than tests).
    
        Change-Id: I22a7efedfbe791d07d5d8efe2ae7d4c0f1a1e6c1
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7882364
        Reviewed-by: Jeremy Roman <jbroman@chromium.org>
        Reviewed-by: Maks Orlovich <morlovich@chromium.org>
        Reviewed-by: Avi Drissman <avi@chromium.org>
        Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
        Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1642494}

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -226,7 +226,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &metrics::structured::kPhoneHubStructuredMetrics,
       &multistep_filter::kMultistepFilter,
       &net::features::kEnableWebTransportDraft07,
-      &net::features::kWaitForFirstPartySetsInit,
       &network::features::kBrowsingTopics,
       &network::features::kInterestGroupStorage,
       &network::features::kSharedStorageAPI,
